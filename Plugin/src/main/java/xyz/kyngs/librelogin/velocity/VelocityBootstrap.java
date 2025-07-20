aa9b142386440b8d4ce9613a335b76ca634394d8
Navio1430 <naviodev@gmail.com>
2025-07-20 04:26:36 +0200

    update VelocityBootstrap

--- Plugin/src/main/java/xyz/kyngs/librelogin/velocity/VelocityBootstrap.java
+++ Plugin/src/main/java/xyz/kyngs/librelogin/velocity/VelocityBootstrap.java
@@ -13,24 +13,23 @@ import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.plugin.PluginContainer;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
-import org.slf4j.Logger;
 import xyz.kyngs.librelogin.api.LibreLoginPlugin;
 import xyz.kyngs.librelogin.api.provider.LibreLoginProvider;
 
 @Plugin(
-        id = "librelogin",
-        name = "LibreLogin",
+        id = "libreloginprod",
+        name = "LibreLoginProd",
         version = "@version@",
-        authors = "kyngs",
+        authors = "sapphirecode",
         dependencies = {
                 @Dependency(id = "floodgate", optional = true),
                 @Dependency(id = "luckperms", optional = true),
                 @Dependency(id = "protocolize", optional = true),
                 @Dependency(id = "redisbungee", optional = true),
+                // TODO: Remove nano limbo plugin support
                 @Dependency(id = "nanolimbovelocity", optional = true)
         }
 )
@@ -40,33 +39,8 @@ public class VelocityBootstrap implements LibreLoginProvider<Player, RegisteredS
     private final VelocityLibreLogin libreLogin;
 
     @Inject
-    public VelocityBootstrap(ProxyServer server, Injector injector, Logger logger, PluginContainer container) {
+    public VelocityBootstrap(ProxyServer server, Injector injector) {
         this.server = server;
-
-//        // This is a very ugly hack to be able to load libraries in the constructor
-//        // We cannot pass this as a parameter to the constructor because the plugin is technically still not loaded
-//        // And, we cannot past the container as a parameter to the constructor because the proxy still did not assign the instance to it.
-//        // So, we have to "mock" the container and pass this as the instance. I'm kinda surprised this works, but in theory could break in the future.
-//        var libraryManager = new VelocityLibraryManager<>(logger, Path.of("plugins", "librelogin"), server.getPluginManager(), new PluginContainer() {
-//            @Override
-//            public PluginDescription getDescription() {
-//                return container.getDescription();
-//            }
-//
-//            @Override
-//            public Optional<?> getInstance() {
-//                return Optional.of(this);
-//            }
-//
-//            @Override
-//            public ExecutorService getExecutorService() {
-//                return Executors.newSingleThreadExecutor();
-//            }
-//        });
-//
-//        logger.info("Loading libraries...");
-//
-//        libraryManager.configureFromJSON();
 
         libreLogin = new VelocityLibreLogin(this);
         injector.injectMembers(libreLogin);
